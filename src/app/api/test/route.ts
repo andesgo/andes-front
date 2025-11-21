565cebd7fea9fa538c18319e04abceaf341bb9f5
Cristobal C <cristobalcordova@hotmail.com>
2025-11-21 16:03:04 -0300

    for deployment

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  return NextResponse.json({ message: "Test working!" })
+  return NextResponse.json({ message: "Test working!!" })
 }
